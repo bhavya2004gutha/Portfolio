e8fe2044b62c2d4ab7918b533f1c619ddd2ba7d6
bhavyagutha2004 <bhavya.gutha@tekreant.com>
2025-08-28 18:26:53 +0530

    add class

---
+++ class.js
@@ -0,0 +1,11 @@
+
+import Square, { Circle } from './shapes.js';
+
+const circle = new Circle(5);
+console.log(circle.area());  
+
+const square = new Square(4);
+console.log(square.area());  
+
+export  default circle;
+
